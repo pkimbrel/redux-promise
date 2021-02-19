1797d6fdf7195466f4080fa654bf387c2e5267be
Paul Kimbrel <paul@paulkimbrel.com>
2021-02-19 09:03:54 -0600

    Added declaration to index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,12 @@
+import { Middleware } from "redux";
+
 export interface PromisePayload<A, P> {
   type: A;
   payload?: P;
   error?: string;
   status: "fetching" | "resolved" | "rejected";
 }
+
+declare const reduxPromise: Middleware<{}, any>;
+
+export default reduxPromise;
